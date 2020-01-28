61d7adee5db83a14d4363f48a0f8f8a8043398b6
cborchar <60355711+cborchar@users.noreply.github.com>
2020-01-28 16:25:50 +0100

    Update One_Peak_BGS.py

--- One_Peak_BGS.py
+++ One_Peak_BGS.py
@@ -11,23 +11,33 @@ from matplotlib import pyplot as plt
 
 def main():
     
+    #number of temperature- and strain-steps:
+    num_Temp = 10
+    num_strain = 10    
+    
+    #initialize lists of parameters:
     gain  = np.ndarray.tolist(np.linspace(1,2,1))
-    bfs   = np.ndarray.flatten(calculate_BFS(np.linspace(0, 40, 10), np.linspace(0, 60, 10)))
-    width = np.ndarray.tolist(np.linspace(2, 4, 10))
-    dfd   = np.ndarray.tolist(np.linspace(10, 11, 1000))
-    noise = 0.01
+    bfs   = np.ndarray.flatten(calculate_BFS(np.linspace(0, 40, num_Temp), np.linspace(0, 60, num_strain)))
+    width = np.ndarray.tolist(np.linspace(100, 1000, 100))
+    noise = np.ndarray.tolist(np.linspace(0.1,1,1))    
+    dfd   = np.ndarray.tolist(np.linspace(0, 1000, 1000))
     
-    gain_array = One_Peak_BGS(gain, bfs, width, dfd, noise)
+    #calculate list of BGS:
+    gain_array = One_Peak_BGS(gain, bfs, width, noise, dfd)
+    gain_array = np.reshape(gain_array, (len(gain), num_Temp, num_strain, len(width), len(noise), len(dfd)))
     
+    gain_array = np.ndarray.tolist(gain_array)
+    
+    #plot of the first graph for test:
     plt.figure()
     plt.title('test')
-    plt.plot(gain_array[0,0,0,:])
+    plt.plot(gain_array[0,0,0,0,:])
     
-    return gain_array, noise, [gain, np.ndarray.flatten(bfs), width, dfd]
+    return gain_array, noise, [gain, bfs, width, dfd]
 
-@jit
-def One_Peak_BGS(gain, bfs, width, dfd, noise_factor):
-    
+#@jit
+def One_Peak_BGS(gain, bfs, width, noise, dfd):
+    #init of g_res:
     g_res = np.zeros((len(gain),len(bfs),len(width),len(dfd)))
     
     i=0
@@ -37,8 +47,11 @@ def One_Peak_BGS(gain, bfs, width, dfd, noise_factor):
             k=0
             for w in width:
                 m=0
-                for d in dfd:
-                    g_res[i, j, k, m] = g/(1 + ((d-b)/(w/2))**2 ) + np.float32(np.random.normal(0,noise_factor))*g
+                for n in noise:
+                    p = 0
+                    for d in dfd:
+                        g_res[i, j, k, m, p] = g/(1 + ((d-b)/(w/2))**2 ) + np.float32(np.random.normal(0,n))*g
+                        p+=1
                     m+=1
                 k+=1
             j+=1
@@ -62,3 +75,6 @@ def calculate_BFS(temperature, strain):
 if __name__ == '__main__':
     main()
     
+
+
+
